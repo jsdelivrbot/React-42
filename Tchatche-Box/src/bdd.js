b3ed4d19de52bac72be97cef6b6336e8d7b6f25c
Echeverria Laurent <40714120+Par0xyste@users.noreply.github.com>
2018-07-14 17:24:11 +0200

    Update bdd.js

--- Tchatche-Box/src/bdd.js
+++ Tchatche-Box/src/bdd.js
@@ -1,9 +1,9 @@
 import Rebase from 're-base';
 
 const BASE = Rebase.createClass({
-    apiKey: "AIzaSyBq1_ZF0ZagODHRRvYVBBXY0gjHE-VclZM",
-    authDomain: "tchatche-box.firebaseapp.com",
-    databaseURL: "https://tchatche-box.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
 });
 
 export default BASE;
